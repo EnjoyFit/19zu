78146bd9cd3e7bef21688eb95b8b20744b5d6234
wzr <710895013@qq.com>
2019-01-04 21:15:44 +0800

    Initial commit

---
+++ src/main/java/com/qfedu/enjoyfitness/EnjoyfitnessApplication.java
@@ -0,0 +1,14 @@
+package com.qfedu.enjoyfitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnjoyfitnessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnjoyfitnessApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/qfedu/enjoyfitness/EnjoyfitnessApplicationTests.java
@@ -0,0 +1,17 @@
+package com.qfedu.enjoyfitness;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EnjoyfitnessApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
